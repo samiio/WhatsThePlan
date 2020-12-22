38c001aef01da4b633a0433e2d6905ca8d5ff168
samiio <sami.ali.96@outlook.com>
2020-12-22 13:55:27 +0400

    added delay to setFabAnimPull()

--- app/src/main/java/uk/ac/reading/student/zj018597/whatstheplan/ui/PlansFragment.java
+++ app/src/main/java/uk/ac/reading/student/zj018597/whatstheplan/ui/PlansFragment.java
@@ -8,13 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -33,7 +30,6 @@ import java.util.Objects;
 
 import uk.ac.reading.student.zj018597.whatstheplan.R;
 import uk.ac.reading.student.zj018597.whatstheplan.db.PlanEntity;
-import uk.ac.reading.student.zj018597.whatstheplan.util.Calculators;
 import uk.ac.reading.student.zj018597.whatstheplan.util.CustomSnackBar;
 import uk.ac.reading.student.zj018597.whatstheplan.util.PlanListAdapter;
 import uk.ac.reading.student.zj018597.whatstheplan.viewmodel.PlanViewModel;
--- app/src/main/java/uk/ac/reading/student/zj018597/whatstheplan/util/Animations.java
+++ app/src/main/java/uk/ac/reading/student/zj018597/whatstheplan/util/Animations.java
@@ -2,6 +2,9 @@ package uk.ac.reading.student.zj018597.whatstheplan.util;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import android.os.Handler;
+import android.os.Looper;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.TranslateAnimation;
@@ -13,16 +16,12 @@ public class Animations {
      * @param floatingActionButton is pulled down after the {@link Snackbar} is gone.
      */
     public static void setFabAnimLift(FloatingActionButton floatingActionButton) {
-
-        final int TIMEOUT_FAB = 3000;
-
         final TranslateAnimation animLift = new TranslateAnimation(
                 Animation.ABSOLUTE, 0,
                 Animation.ABSOLUTE, 0,
                 Animation.ABSOLUTE, 0,
                 Animation.ABSOLUTE, -100
         );
-
         animLift.setDuration(100);
 
         final TranslateAnimation animPause = new TranslateAnimation(
@@ -31,8 +30,7 @@ public class Animations {
                 Animation.ABSOLUTE, 0,
                 Animation.ABSOLUTE, 0
         );
-
-        animPause.setDuration(TIMEOUT_FAB);
+        animPause.setDuration(3000);
 
         final AnimationSet animSet = new AnimationSet(true);
         animSet.addAnimation(animLift);
@@ -41,12 +39,7 @@ public class Animations {
         floatingActionButton.startAnimation(animSet);
     }
 
-    /**
-     * Sets the {@link FloatingActionButton} the initial animation when an item is deleted.
-     * @param floatingActionButton is is moved up when to make space for the {@link Snackbar}.
-     */
     public static void setFabAnimPull(FloatingActionButton floatingActionButton) {
-
         final TranslateAnimation animPull = new TranslateAnimation(
                 Animation.ABSOLUTE, 0,
                 Animation.ABSOLUTE, 0,
@@ -54,7 +47,8 @@ public class Animations {
                 Animation.ABSOLUTE, 1
         );
         animPull.setDuration(500);
-        floatingActionButton.setAnimation(animPull);
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> floatingActionButton.setAnimation(animPull), 200);
     }
 
 }
