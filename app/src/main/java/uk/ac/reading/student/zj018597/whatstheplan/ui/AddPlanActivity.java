7e5df7cced991697bd4c6195d4592e897817e64e
samiio <sami.ali.96@outlook.com>
2020-12-22 18:59:14 +0400

    clean up Add* activities

--- app/src/main/java/uk/ac/reading/student/zj018597/whatstheplan/ui/AddPlanActivity.java
+++ app/src/main/java/uk/ac/reading/student/zj018597/whatstheplan/ui/AddPlanActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,32 +12,34 @@ import uk.ac.reading.student.zj018597.whatstheplan.R;
 import uk.ac.reading.student.zj018597.whatstheplan.db.PlanEntity;
 
 /**
- * Activity which creates and adds a new {@link PlanEntity}.
+ * Launched to create and add a new {@link PlanEntity}.
  */
 public class AddPlanActivity extends AppCompatActivity {
 
-    public static final String EXTRA_REPLY_PLAN = "whatstheplan.plan.REPLY";
-
     private EditText etName;
+    public static final String EXTRA_REPLY_PLAN = "whatstheplan.plan.REPLY";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plan);
         etName = findViewById(R.id.et_plan);
+        Button button = findViewById(R.id.btn_add_plan);
+        button.setOnClickListener(new AddNewPlanClick());
+    }
 
-        final Button button = findViewById(R.id.btn_add_plan);
-        button.setOnClickListener(view -> {
+    private class AddNewPlanClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
             Intent replyIntent = new Intent();
             if (TextUtils.isEmpty(etName.getText())) {
                 setResult(RESULT_CANCELED, replyIntent);
             } else {
-                String word = etName.getText().toString();
-                replyIntent.putExtra(EXTRA_REPLY_PLAN, word);
+                String plan = etName.getText().toString();
+                replyIntent.putExtra(EXTRA_REPLY_PLAN, plan);
                 setResult(RESULT_OK, replyIntent);
             }
             finish();
-        });
+        }
     }
-
 }
--- app/src/main/java/uk/ac/reading/student/zj018597/whatstheplan/ui/AddRestaurantActivity.java
+++ app/src/main/java/uk/ac/reading/student/zj018597/whatstheplan/ui/AddRestaurantActivity.java
@@ -1,38 +1,46 @@
 package uk.ac.reading.student.zj018597.whatstheplan.ui;
 
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import uk.ac.reading.student.zj018597.whatstheplan.R;
+import androidx.appcompat.app.AppCompatActivity;
 
+import uk.ac.reading.student.zj018597.whatstheplan.R;
+import uk.ac.reading.student.zj018597.whatstheplan.db.RestaurantEntity;
+
+/**
+ * Launched to create and add a new {@link RestaurantEntity}.
+ */
 public class AddRestaurantActivity extends AppCompatActivity {
 
-    public static final String EXTRA_REPLY_RESTAURANT = "whatstheplan.restaurant.REPLY";
-
     private EditText etName;
+    public static final String EXTRA_REPLY_RESTAURANT = "whatstheplan.restaurant.REPLY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_restaurant);
-
         etName = findViewById(R.id.et_restaurant);
+        Button button = findViewById(R.id.btn_add_restaurant);
+        button.setOnClickListener(new AddNewRestaurantClick());
+    }
 
-        final Button button = findViewById(R.id.btn_add_restaurant);
-        button.setOnClickListener(view -> {
+    private class AddNewRestaurantClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
             Intent replyIntent = new Intent();
             if (TextUtils.isEmpty(etName.getText())) {
                 setResult(RESULT_CANCELED, replyIntent);
             } else {
-                String word = etName.getText().toString();
-                replyIntent.putExtra(EXTRA_REPLY_RESTAURANT, word);
+                String restaurant = etName.getText().toString();
+                replyIntent.putExtra(EXTRA_REPLY_RESTAURANT, restaurant);
                 setResult(RESULT_OK, replyIntent);
             }
             finish();
-        });
+        }
     }
 }
